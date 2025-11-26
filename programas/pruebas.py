2815fc5150f533a6954457ccc0d99ab77cc724b7
samzhand <edgar18tor@gmail.com>
2025-11-25 23:29:56 -0600

    ultimo

--- programas/pruebas.py
+++ programas/pruebas.py
@@ -29,6 +29,9 @@ cifrado = "Hmgh cmnnl dlj bgholj pldizj, dl dimz b al zmnsolgjlj nmgjhbcwlj lubi
 llave = 'bscalpkwivrdnzmufojhgxtyqe'
 dic1 = carga_dic('../diccionarios/dic_fran')
 
+texto2="Qy iuoxukdldut mb wydkdt gywdz kzqut eb'dq k'ysdl mz wydkdtk jqytik ub tudwk. Mytk qzk mzbn iyk, mzbn lpxzk mz tblwdoztlk kz mdkldtsbztl : qzk kbiwzk, xwdtidxyqzoztl qz sqbiukz zl qz cwbilukz, xqbk yjutmytlk mytk qz wydkdt jqyti, zl qzk gdlyodtzk (yidmz cuqdebz zl gdlyodtz J6). Iz mzwtdzw zt ebytldlz ebd t'zkl kbwxykkz ebz xyw qzk cwbdlk kzik zl qzk cwbdlk lwuxdiybn iuooz q'yguiyl, qy jytytz, q'ytutz, qy supygz zl qy oytsbz. Ky wdifzkkz zt kbiwzk zt cydl q'bt mzk cwbdlk qzk xqbk iyquwdebzk. Qzk wydkdtk ibqldgzk mytk qzk wzsdutk cwudmzk utl lztmytiz y iutlztdw oudtk mz kbiwzk ebz izbn ibqldgzk mytk bt kuq ifybm zl kzi. Xywod qzk odtzwybn, qz xulykkdbo zkl qz xqbk yjutmytl zl kz wzlwubgz zt xqbk swytmz ebytldlz mytk qz wydkdt tudw ; lytmdk ebz qz oystzkdbo zl qz iyqidbo kz lwubgztl zt ebytldlzk oumzwzzk zl kutl xqbk yjutmytlk mytk qzk wydkdtk jqytik. Q'bldqdkyldut mytk q'uwsytdkoz mz iz mzwtdzw odtzwyq t'zkl xyk ybkkd doxuwlytlz ebz izqqz ebd xwugdztl mzk xwumbdlk qydldzwk ub m'yblwzk yqdoztlk ebd kutl btz juttz kubwiz mbmdl odtzwyq."
+llave2="yjimzcsfdhrqotuxewklbgvnpa"
+
 """Hmgh cmnnl dlj bgholj pldizj, dl dimz b al zmnsolgjlj nmgjhbcwlj lubijjlj. 
 Clj dmzkj umidj jlzjisdlj bgy xisobhimzj bialzh dl dimz b jl aioiklo abzj d'msjcgoihl, mg fgbza jmz 
 cwbnu xijgld ljh msjhogl. Db nbvlgol ubohil al jb cwbjjl jl alomgdbzh db zgih, idj d'bialzh uoljfgl b jlzhio 
@@ -84,6 +87,6 @@ for p in dic1:
 
 #and p[2] in posE
 #and p[3]in posG
-
-out=open("../resultado/resultado","w",encoding="utf-8")
-out.write(descifra_sustitucion(cifrado,llave))
+print(descifra_sustitucion(texto2,llave2))
+#out=open("../resultado/resultado","w",encoding="utf-8")
+#out.write(descifra_sustitucion(cifrado,llave))
